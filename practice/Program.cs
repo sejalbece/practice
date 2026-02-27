dbb0605f907370fe1bc6808d5b7bcbe5f6604672
Sejalben Harshadbhai Jiyani <sejalbenharshadbhai.jiyani@algomabrampton.ca>
2026-02-27 10:17:27 -0500

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string"); 
+            string s1 = Console.ReadLine();
+            string temp = s1;
+            s1 = s1.Replace(" ", string.Empty);
+            Console.WriteLine($"The original string is: {temp}");
+            
+            while (s1.Length > 0)
+            {
+                Console.Write(s1[0] + " : ");
+                int count = 0;
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if (s1[0] == s1[i])
+                    { 
+                        count++;
+                    }                    
+                }
+                Console.WriteLine(count);
+                s1 = s1.Replace(s1[0].ToString(), string.Empty);
+
+            }    
+
+            
+            
+           
+
+            Console.ReadLine();
+        }
+    }
+}
